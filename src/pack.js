02630d4faa3e8c547e0a83e52d17888977c0ca5d
ianmsfvenom <57237342+ianmsfvenom@users.noreply.github.com>
2021-01-07 00:26:40 -0300

    Update pack.js

--- src/pack.js
+++ src/pack.js
@@ -1,9 +1,25 @@
 const pack = (prefix) => {
-    return `PASTA DO MEGA COM DIVERSOS VIDEOS E FOTOS PORN:\n
-\n🔰 FOTOS DE HENTAI 720MB: https://encurta.eu/YU7k 🔰
-\n🔰 PRINCESS STARLING(ONLY FANS)8.36GB: https://encurta.eu/cBiTfDBd 🔰
-\n🔰 ELLES CLUB 28.68GB: https://encurta.eu/ESTyP 🔰
-\n🔰 COCONUTKITTY(ONLY FANS)7.69GB: https://encurta.eu/U2I5 🔰
+    return `PASTA DO MEGA COM DIVERSOS VIDEOS E FOTOS PORN:
+
+🔰 FOTOS DE HENTAI 720MB: https://encurta.eu/YU7k 🔰
+
+🔰 PRINCESS STARLING(ONLY FANS)8.36GB: https://encurta.eu/cBiTfDBd 🔰
+
+🔰 ELLES CLUB 28.68GB: https://encurta.eu/ESTyP 🔰
+
+🔰 COCONUTKITTY(ONLY FANS)7.69GB: https://encurta.eu/U2I5🔰
+
+🔰 Belle delphine : https://photos.app.goo.gl/Jr4Qk1dFSJepPdRc7 🔰
+
+🔰 Pack1: https://photos.app.goo.gl/phyRpNFLcAtsknfJ7 🔰
+
+🔰 Kitty kum: https://photos.app.goo.gl/gNxLbJHGVzeWY9iS9 🔰
+
+🔰 Misaki Your Waifu: https://photos.app.goo.gl/M9nyHdonAJu5GRjZ9 🔰
+
+🔰 pack: https://photos.app.goo.gl/SrogbCbnanL2PJQn7 🔰
+
+🔰 Love Lilah : https://photos.app.goo.gl/TBbkjGGdAVHjPFt5A 🔰
 `
 }
 exports.pack = pack
